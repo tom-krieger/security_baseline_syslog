132adbafdff38c09b1859036388eedbe7330c01e
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-27 13:16:13 +0200

    added facts

--- spec/classes/security_baseline_syslog_spec.rb
+++ spec/classes/security_baseline_syslog_spec.rb
@@ -4,6 +4,25 @@ describe 'security_baseline_syslog' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      os_facts.merge(
+        'security_baseline_syslog' => {
+          'rsyslog' => {
+            'filepermissions' => '0640',
+            'loghost' => true,
+            'package' => true,
+            'remotesyslog' => 'none',
+            'service' => 'enabled'
+          },
+          'syslog-ng' => {
+            'filepermissions' => 'none',
+            'loghost' => false,
+            'package' => false,
+            'remotesyslog' => 'none',
+            'service' => 'disabled'
+          },
+          'syslog_installed' => true,
+        }
+      )
 
       it { is_expected.to compile }
     end
