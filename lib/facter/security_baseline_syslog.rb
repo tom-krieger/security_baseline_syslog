e9f4d7cc20cbb7070e97928e6782d6cc61e199b9
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-27 10:18:30 +0200

    removed strip

--- lib/facter/security_baseline_syslog.rb
+++ lib/facter/security_baseline_syslog.rb
@@ -36,7 +36,7 @@ Facter.add(:security_baseline_syslog) do
     syslog_ng['package'] = check_package_installed('syslog-ng')
     val = Facter::Core::Execution.exec('grep ^options /etc/syslog-ng/syslog-ng.conf 2>/dev/null').match(%r{perm\((\d+)\)}).strip
     syslog_ng['filepermissions'] = check_value_string(val, 'none')
-    val = Facter::Core::Execution.exec('grep destination logserver /etc/syslog-ng/syslog-ng.conf 2>/sdev/null').match(%r{tcp\((.*)\)}).strip
+    val = Facter::Core::Execution.exec('grep destination logserver /etc/syslog-ng/syslog-ng.conf 2>/sdev/null').match(%r{tcp\((.*)\)})
     logserv = check_value_string(val, 'none')
     val = Facter::Code::Execution.exec('grep "log.*{.*source(src);.*destination(logserver);.*};" /etc/syslog-ng/syslog-ng.conf 2>/dev/null')
     logsend = check_value_string(val, 'none')
